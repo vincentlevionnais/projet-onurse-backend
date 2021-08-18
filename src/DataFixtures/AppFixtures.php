5aef65b4d41a85fa963e907ef050d165a1c3bb05
vincentlevionnais <vincentlevionnais@hotmail.fr>
2021-08-18 14:19:52 +0200

    adjust fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -24,7 +24,7 @@ class AppFixtures extends Fixture
          $nurse->setEmail('alainfirmier@liberal.com');
          $nurse->setPassword('$2y$13$j.gXly4IYfmvqYnL/rEDWuIta7DuUmfQcj9RiYiLIeGWvRIv045Y2');
          //password de test hashé: "liberal"
-         $nurse->setCreatedAt(new DateTimeImmutable("2021-08-10 10-15-00"));
+         $nurse->setCreatedAt(new \DateTimeImmutable('2021-08-10 10:15:00'));
          //$nurse->setUpdatedAt('');
  
          
@@ -39,15 +39,15 @@ class AppFixtures extends Fixture
          $patient->setPhone('0607080910');
          $patient->setCompleteAdress('10 Rue de la source 35400 Saint Malo');
          $patient->setInformationAdress('Katell');
-         $patient->setNir(252043505500513);
+         $patient->setNir('252043505500513');
          $patient->setDoctorName('Meredith Grey');
          $patient->setMutualName('France Mutuelle');
-         $patient->setMutualNumberAmc(44223544);
+         $patient->setMutualNumberAmc('44223544');
          //$patient->setMutualCardImage('');
          $patient->setPathology('diabète type 2');
          //$patient->setNote('');
          $patient->setTrustedPerson('MARI Gérard Mensouaf 0680089090');
-         $patient->setCreatedAt(new DateTimeImmutable("2021-08-15 12:00:00"));
+         $patient->setCreatedAt(new \DateTimeImmutable('2021-08-15 12:00:00'));
          //$patient->setUpdatedAt('');
 
 
