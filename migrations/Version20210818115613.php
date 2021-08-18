bfa4a2392c153f328a65ce7d764bc6a05b32049f
vincentlevionnais <vincentlevionnais@hotmail.fr>
2021-08-18 13:59:33 +0200

    migration entities

---
+++ migrations/Version20210818115613.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210818115613 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE appointment (id INT AUTO_INCREMENT NOT NULL, datetime_start DATETIME NOT NULL, datetime_end DATETIME NOT NULL, reason VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE nurse (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(64) NOT NULL, lastname VARCHAR(64) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(64) NOT NULL, phone INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE patient (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(64) NOT NULL, lastname VARCHAR(64) NOT NULL, birthdate DATE DEFAULT NULL, nir INT DEFAULT NULL, phone INT NOT NULL, complete_adress VARCHAR(255) NOT NULL, information_adress VARCHAR(255) DEFAULT NULL, note LONGTEXT DEFAULT NULL, doctor_name VARCHAR(255) DEFAULT NULL, mutual_name VARCHAR(255) DEFAULT NULL, mutual_number_amc VARCHAR(255) DEFAULT NULL, pathology VARCHAR(255) DEFAULT NULL, trusted_person LONGTEXT DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE appointment');
+        $this->addSql('DROP TABLE nurse');
+        $this->addSql('DROP TABLE patient');
+    }
+}
