ab3a5d93b745932977f1505a7ec8a838e2a39771
vincentlevionnais <vincentlevionnais@hotmail.fr>
2021-08-26 12:42:03 +0200

    conditions of access to nurse data

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Nurse;
-use App\Repository\NurseRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,28 +16,29 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class NurseController extends AbstractController
 {
-
-    /**
-     * Get Nurses 
-     * 
-     * @Route("/api/nurses", name="api_nurses_get", methods="GET")
-     */
-    public function browse(NurseRepository $nursesRepository): Response
-    {
-        $nurses = $nursesRepository->findAll();
-
-        // Resquest to Symfony to "serialize" entities in form of JSON
-        return $this->json($nurses, 200, [], ['groups' => 'nurse_get']);
-    }
-
     
      /**
      * Get one nurse by id (see my account)
      * 
      * @Route("/api/nurses/{id<\d+>}", name="api_nurse_get_item", methods="GET")
      */
-    public function read(Nurse $nurse): Response
-    {       
+    public function read(Nurse $nurse = null): Response
+    {     
+        if($nurse === null)
+        {
+            return new JsonResponse(["message" => "Compte utilisateur non trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        $user = $this->getUser();
+        $userId = $user->getId();
+
+        $nurseId = $nurse->getId();
+
+        if($userId != $nurseId)
+        {
+            return new JsonResponse(["message" => "Compte utilisateur non trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
         return $this->json($nurse, Response::HTTP_OK, [], ['groups' => 'nurse_get']);
     }
 
@@ -52,7 +52,18 @@ class NurseController extends AbstractController
     {
         // if nurse not found
         if ($nurse === null) {
-            return new JsonResponse(["message" => "Compte non trouvé"], Response::HTTP_NOT_FOUND);
+            return new JsonResponse(["message" => "Compte utilisateur non trouvé"], Response::HTTP_NOT_FOUND);
+        }
+
+        // if user is not this nurse
+        $user = $this->getUser();
+        $userId = $user->getId();
+
+        $nurseId = $nurse->getId();
+
+        if($userId != $nurseId)
+        {
+            return new JsonResponse(["message" => "Compte utilisateur non trouvé"], Response::HTTP_NOT_FOUND);
         }
 
         // else we get the data's request
@@ -71,30 +82,13 @@ class NurseController extends AbstractController
         // Errors display
         if (count($errors) > 0) {
 
-            //!todo mettre en anglais
-            // Objectif : create this out format 
-            // {
-            //     "errors": {
-            //         "title": [
-            //             "Cette valeur ne doit pas être vide."
-            //         ],
-            //             "releaseDate": [
-            //             "Cette valeur doit être de type string."
-            //         ],
-            //         "rating": [
-            //             "Cette chaîne est trop longue. Elle doit avoir au maximum 1 caractère.",
-            //             "Cette valeur doit être l'un des choix proposés."
-            //         ]
-            //     }
-            // }
-
             // creating an errors array
             $newErrors = [];
 
             foreach ($errors as $error) {
                 // We push in an arrays
                 // = similar tu the structure of Flash Messages
-                // We pus the message, ti the key that contains the property               
+                // We push the message, to the key that contains the property               
                 $newErrors[$error->getPropertyPath()][] = $error->getMessage();
             }
 
@@ -102,7 +96,7 @@ class NurseController extends AbstractController
         }
 
         // Database recording
-         $entityManager->flush();
+        //$entityManager->flush();
 
         //!todo Conditionner le message de retour au cas où
         // l'entité ne serait pas modifiée
@@ -119,7 +113,7 @@ class NurseController extends AbstractController
     {
         $jsonContent = $request->getContent();
 
-        // Désérialise the JSON to the new entity Nurse
+        // Deserialise the JSON to the new entity Nurse
         // @see https://symfony.com/doc/current/components/serializer.html#deserializing-an-object
         $nurse = $serializer->deserialize($jsonContent, Nurse::class, 'json');
 
@@ -133,27 +127,18 @@ class NurseController extends AbstractController
         }
 
 
-        // On hash le mot de passe
         $hashedPassword = $userPasswordHasher->hashPassword($nurse, $nurse->getPassword());
-        // On le remet dans $user->password
-        $nurse->setPassword($hashedPassword);
-        //dd($nurse);
 
-        // We are preparing to persist in Database, and flush
+        $nurse->setPassword($hashedPassword);
+
         $entityManager->persist($nurse);
         $entityManager->flush();
 
-        // REST ask us a 201 status and a header Location: url
-        //! Si on le fait "à la mano" voir autre manière de faire ?
+
         return $this->json(
-            // the Nurse we return in JSON at the front
             $nurse,
-            // The status code
             Response::HTTP_CREATED,
-            // The header Location + the URL of the created ressource
             ['Location' => $this->generateUrl('api_nurse_get_item', ['id' => $nurse->getId()])],
-            //!todo à vérifier après avoir mis les relations sur les entités
-            //! Le groupe de sérialisation pour que $patient soit sérialisé sans erreur de référence circulaire
             ['groups' => 'nurse_get']
         );
     }
@@ -165,13 +150,19 @@ class NurseController extends AbstractController
      */
     public function delete(Nurse $nurse = null, EntityManagerInterface $entityManager)
     {
+        if($nurse === null)
+        {
+            return new JsonResponse(["message" => "Compte utilisateur non trouvé"], Response::HTTP_NOT_FOUND);
+        }
 
-        //Errors display
-        if (null === $nurse) {
+        $user = $this->getUser();
+        $userId = $user->getId();
 
-            $error = 'Ce compte n\'existe pas';
+        $nurseId = $nurse->getId();
 
-            return $this->json(['error' => $error], Response::HTTP_NOT_FOUND);
+        if($userId != $nurseId)
+        {
+            return new JsonResponse(["message" => "Compte utilisateur non trouvé"], Response::HTTP_NOT_FOUND);
         }
 
         $entityManager->remove($nurse);
